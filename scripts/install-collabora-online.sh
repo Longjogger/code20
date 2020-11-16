a678ccac3afd00ea17f2f768d2361e10c2dd718a
Longjogger <Longjogger@users.noreply.github.com>
2020-11-16 13:24:14 +0100

    Ubuntu 20.04 instead 18.04

--- scripts/install-collabora-online.sh
+++ scripts/install-collabora-online.sh
@@ -22,7 +22,7 @@ apt-get -y install openssh-client
 apt-get -y install curl
 
 # Add Collabora repos
-echo "deb https://collaboraoffice.com/repos/CollaboraOnline/CODE-ubuntu1804 /" > /etc/apt/sources.list.d/collabora.list
+echo "deb https://collaboraoffice.com/repos/CollaboraOnline/CODE-ubuntu2004 /" > /etc/apt/sources.list.d/collabora.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 6CCEA47B2281732DF5D504D00C54D189F4BA284D
 apt-get update
 
